eea0e5be5cd4dd7745de3c9c333a6aac350e8899
haya <91950797+nonosaku@users.noreply.github.com>
2022-01-21 23:05:16 +0900

    Update digital.js

--- digital.js
+++ digital.js
@@ -1,38 +1,38 @@
 const clock = () => {
-  // 現在の日時・時刻の情報を取得
-  const d = new Date();
+  //日時・時刻
+  const time = new Date();
 
-  // 年を取得
-  let year = d.getFullYear();
-  // 月を取得
-  let month = d.getMonth() + 1;
-  // 日を取得
-  let date = d.getDate();
-  // 曜日を取得
-  let dayNum = d.getDay();
-  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  //年を取得
+  let year = time.getFullYear();
+  //月を取得
+  let month = time.getMonth() + 1;
+  //日を取得
+  let date = time.getDate();
+  //曜日を取得
+  let dayNum = time.getDay();
+  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let day = weekday[dayNum];
-  // 時を取得
-  let hour = d.getHours();
-  // 分を取得
-  let min = d.getMinutes();
-  // 秒を取得
-  let sec = d.getSeconds();
+  //時を取得
+  let hour = time.getHours();
+  //分を取得
+  let min = time.getMinutes();
+  //秒を取得
+  let sec = time.getSeconds();
 
   // 1桁の場合は0を足して2桁に
-  month = month < 10 ? "0" + month : month;
-  date = date < 10 ? "0" + date : date;
-  hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
+  //month = month < 10 ? "0" + month : month;
+  //date = date < 10 ? "0" + date : date;
+  //hour = hour < 10 ? "0" + hour : hour;
+  //min = min < 10 ? "0" + min : min;
+  //sec = sec < 10 ? "0" + sec : sec;
 
   // 日付・時刻の文字列を作成
   let today = `${year}.${month}.${date} ${day}`;
   let time = `${hour}:${min}:${sec}`;
 
   // 文字列を出力
-  document.querySelector(".clock-date").innerText = today;
-  document.querySelector(".clock-time").innerText = time;
+  document.querySelector("div.date").innerText = today;
+  document.querySelector("div.time").innerText = time;
 };
 
 // 1秒ごとにclock関数を呼び出す
