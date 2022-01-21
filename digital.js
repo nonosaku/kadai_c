4f039bb4fad127e3102db5d1933b6050e1976587
haya <91950797+nonosaku@users.noreply.github.com>
2022-01-21 23:45:16 +0900

    Update digital.js

--- digital.js
+++ digital.js
@@ -1,8 +1,7 @@
 const clock = () => {
   //日時・時刻
   const time = new Date();
-  //年を取得
-  let year = time.getFullYear();
+  
   //月を取得
   let month = time.getMonth() + 1;
   //日を取得
@@ -15,19 +14,17 @@ const clock = () => {
   let hour = time.getHours();
   //分を取得
   let min = time.getMinutes();
-  //秒を取得
-  let sec = time.getSeconds();
+ 
 
   // 1桁の場合は0を足して2桁に
-  //month = month < 10 ? "0" + month : month;
-  //date = date < 10 ? "0" + date : date;
-  //hour = hour < 10 ? "0" + hour : hour;
-  //min = min < 10 ? "0" + min : min;
-  //sec = sec < 10 ? "0" + sec : sec;
-
+  month = month < 10 ? "0" + month : month;
+  date = date < 10 ? "0" + date : date;
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+ 
   // 日付・時刻の文字列を作成
-  let today = `${year}.${month}.${date} ${day}`;
-  let time = `${hour}:${min}:${sec}`;
+  let today = `${month}.${date} ${day}`;
+  let time = `${hour}:${min}:`;
 
   // 文字列を出力
   document.querySelector("div.date").innerText = today;
