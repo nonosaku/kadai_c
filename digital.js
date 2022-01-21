2a912e954f487f6237801625c3318bed63fa312f
haya <91950797+nonosaku@users.noreply.github.com>
2022-01-21 22:54:05 +0900

    Update digital.js

--- digital.js
+++ digital.js
@@ -1,5 +1,7 @@
-const clock=()=>{// 現在の日時・時刻の情報を取得
-  const d=new Date();
+const clock = () => {
+  // 現在の日時・時刻の情報を取得
+  const d = new Date();
+
   // 年を取得
   let year = d.getFullYear();
   // 月を取得
@@ -8,7 +10,7 @@ const clock=()=>{// 現在の日時・時刻の情報を取得
   let date = d.getDate();
   // 曜日を取得
   let dayNum = d.getDay();
-  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   let day = weekday[dayNum];
   // 時を取得
   let hour = d.getHours();
@@ -17,10 +19,21 @@ const clock=()=>{// 現在の日時・時刻の情報を取得
   // 秒を取得
   let sec = d.getSeconds();
 
+  // 1桁の場合は0を足して2桁に
+  month = month < 10 ? "0" + month : month;
+  date = date < 10 ? "0" + date : date;
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  // 日付・時刻の文字列を作成
   let today = `${year}.${month}.${date} ${day}`;
   let time = `${hour}:${min}:${sec}`;
-  
-  document.querySelector("p.date").innerText = today;
-  document.querySelector("p.time").innerText = time;
+
+  // 文字列を出力
+  document.querySelector(".clock-date").innerText = today;
+  document.querySelector(".clock-time").innerText = time;
 };
+
+// 1秒ごとにclock関数を呼び出す
 setInterval(clock, 1000);
