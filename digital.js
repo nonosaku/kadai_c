0af4f5e0fbcfb3af7ee2f73a18cb1164a423e13e
haya <91950797+nonosaku@users.noreply.github.com>
2022-01-29 18:16:43 +0900

    Update digital.js

--- digital.js
+++ digital.js
@@ -2,13 +2,13 @@ function DigitalClock(){
 //日時・時刻
   var now = new Date();
   //月を取得
-  var month = now.getMonth()+1;
+  //var month = now.getMonth()+1;
   //日を取得
-  var date = now.getDate();
+  //var date = now.getDate();
   //曜日を取得
-  var dayNum = now.getDay();
-  var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var day = weekday[dayNum];
+  //var dayNum = now.getDay();
+  //var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  //var day = weekday[dayNum];
   //時を取得
   var hour = now.getHours();
   //分を取得
@@ -17,10 +17,10 @@ function DigitalClock(){
   var second = now.getSeconds();
   
   //文字列
- var doc= month + " " + date + " " + day;
+ //var doc= month + " " + date + " " + day;
  var doc2=hour + ":" + minute + ":" + second;
   
-document.getElementById("digital_clock").innerHTML=doc;
-document.getElementById("digital_clock2").innerHTML=doc2;
+//document.getElementById("digital_clock").innerHTML=doc;
+document.getElementById("digital_clock").innerHTML=doc2;
 }
 setInterval('DigitalClock()',1000);
